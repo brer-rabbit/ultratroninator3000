1fc5aa249618b11f2243a5477917c1d897e3dbce
Kyle Farrell <ultratroninator3000@patentinvestor.com>
2021-10-19 06:44:28 +0100

    got some Tempest flipper movement down and it's kind of silly. But hey, that's what I'm here for.

--- src/tempest/model.c
+++ src/tempest/model.c
@@ -36,13 +36,27 @@ static display_type get_blue_display(struct display_strategy *display_strategy, 
 static display_type get_green_display(struct display_strategy *display_strategy, display_value *value, ht16k33blink_t *blink, ht16k33brightness_t *brightness);
 static display_type get_leds_display(struct display_strategy *display_strategy, display_value *value, ht16k33blink_t *blink, ht16k33brightness_t *brightness);
 
+struct flipper;
+typedef enum { DISPLAY_RED, DISPLAY_BLUE, DISPLAY_GREEN } display_t;
+static void display_flipper(struct flipper*, display_t, display_value*);
 
-typedef enum { INACTIVE, SPAWNING, ACTIVE, DESTROYED } flipper_state_t;
+// abuse note: ordering is important, movement of flippers iterates the enum
+typedef enum { INACTIVE,  // not on game board
+               SPAWNING,  // on the board, viewable, can't be hit
+               FLIPPING_FROM1, // leaving a space while moving, can't hit player
+               FLIPPING_FROM2, // leaving a space while moving, can't hit player
+               FLIPPING_FROM3, // leaving a space while moving, can't hit player
+               FLIPPING_TO3, // entering a space while moving, can't hit player
+               FLIPPING_TO2, // entering a space while moving, can't hit player
+               FLIPPING_TO1, // entering a space while moving, can't hit player
+               ACTIVE,    // moving across the board, can be hit or hit player
+               DESTROYED  // blown to pieces, removed from game board
+} flipper_state_t;
 
 struct flipper {
   int position;
   int depth;
-  int movement_direction; // direction the flipper takes during ascending
+  int movement_direction; // direction the flipper takes during movement: cw (-11), ccw (1)
   flipper_state_t flipper_state;
   int intra_depth_timer; // timer for moving within the current depth
   int next_depth_timer; // timer for advancing to next depth (well, depth -1, surfacing?)
@@ -63,17 +77,18 @@ struct player {
 #define MAX_PLAYFIELD_ARRAY_SIZE 28
 #define MAX_FLIPPERS 16
 
-static const int default_flipper_move_timer = 10;
-static const int default_next_depth_timer = 300;
+static const int default_flipper_move_timer = 50;
+static const int default_flipper_flip_timer = 5;
+static const int default_next_depth_timer = 250;
 static const int default_spawn_next_nasty_timer = 30;
 static const int default_max_spawning_nasties = 2;
-static const int default_spawn_movement_timer = 5;  // spawn timer + randomness
-static const int default_spawn_movement_timer_randomness = 5;
+static const int default_spawn_move_timer = 5;  // spawn timer + randomness
+static const int default_spawn_move_timer_randomness = 5;
 
 
 struct playfield {
   int num_arrays;
-  int max_index_by_array[MAX_PLAYFIELD_NUM_ARRAYS];
+  int size_per_array[MAX_PLAYFIELD_NUM_ARRAYS];
   int arrays[MAX_PLAYFIELD_NUM_ARRAYS][MAX_PLAYFIELD_ARRAY_SIZE];
   int circular; // boolean- should it wrap around?  applies to all arrays
   int spawn_next_nasty_timer; // timer to spawn next enemy
@@ -127,15 +142,15 @@ void move_player(struct model *this, int8_t direction) {
 
   if (direction > 0) {
     this->player.position++;
-    if (this->player.position >= this->playfield.max_index_by_array[0]) {
-      this->player.position = this->playfield.circular ? 0 : this->playfield.max_index_by_array[0] - 1;
+    if (this->player.position >= this->playfield.size_per_array[0] - 1) {
+      this->player.position = this->playfield.circular ? 0 : this->playfield.size_per_array[0] - 1;
     }
   }
   else if (direction < 0) {
     this->player.position--;
     if (this->player.position < 0) {
       this->player.position = this->playfield.circular ?
-	this->playfield.max_index_by_array[0] - 1 : 0;
+	this->playfield.size_per_array[0] - 1 : 0;
     }
   }
 
@@ -208,26 +223,62 @@ static void move_flippers(struct model *this) {
         printf("flipper %d promoted to depth %d\n", i, this->playfield.flippers[i].depth);
         this->playfield.flippers[i].intra_depth_timer = default_flipper_move_timer;
         // start it randomly somewhere on the next depth
-        // TODO: map this to a logical position
-        this->playfield.flippers[i].position = rand() % this->playfield.max_index_by_array[this->playfield.flippers[i].depth];
+        // TODO: map this to a logical position, not random
+        this->playfield.flippers[i].position =
+          this->playfield.flippers[i].position *
+          this->playfield.size_per_array[this->playfield.flippers[i].depth] /
+          this->playfield.size_per_array[this->playfield.flippers[i].depth + 1];
+          
       }
       else if (--this->playfield.flippers[i].intra_depth_timer == 0) {
-        // reset timer, move flipper, check boundary of current depth
-        this->playfield.flippers[i].intra_depth_timer = default_flipper_move_timer;
+        // reset timer to the flipper flip timer
+        this->playfield.flippers[i].intra_depth_timer = default_flipper_flip_timer;
+        // we're ACTIVE and our timer expired, start transition out of
+        // this position
+        this->playfield.flippers[i].flipper_state = FLIPPING_FROM1;
+
+      }
+      break;
+
+    case FLIPPING_FROM3:
+      // on this first clock of the state,
+      // actually move to the next position, checking for boundary conditions
+      if (this->playfield.flippers[i].intra_depth_timer == default_flipper_flip_timer) {
         this->playfield.flippers[i].position +=
           this->playfield.flippers[i].movement_direction;
-        if (this->playfield.flippers[i].position > this->playfield.max_index_by_array[this->playfield.flippers[i].depth]) {
+        if (this->playfield.flippers[i].position >= this->playfield.size_per_array[this->playfield.flippers[i].depth]) {
           // don't flip past the array end
           this->playfield.flippers[i].position = 0;
         }
         else if (this->playfield.flippers[i].position < 0) {
           this->playfield.flippers[i].position =
-            this->playfield.max_index_by_array[this->playfield.flippers[i].depth];
+            this->playfield.size_per_array[this->playfield.flippers[i].depth] - 1;
         }
         printf("flipper %d moved to position %d depth %d\n", i,
                this->playfield.flippers[i].position, this->playfield.flippers[i].depth);
       }
 
+    // fallthrough
+    case FLIPPING_FROM1:
+    case FLIPPING_FROM2:
+    case FLIPPING_TO3:
+    case FLIPPING_TO2:
+      if (--this->playfield.flippers[i].intra_depth_timer == 0) {
+        this->playfield.flippers[i].intra_depth_timer = default_flipper_flip_timer;
+        this->playfield.flippers[i].flipper_state++;
+      }
+      break;
+
+    case FLIPPING_TO1:
+      if (--this->playfield.flippers[i].intra_depth_timer == 0) {
+        this->playfield.flippers[i].intra_depth_timer = default_flipper_move_timer;
+        this->playfield.flippers[i].flipper_state = ACTIVE;
+      }
+      break;
+
+    case DESTROYED:
+    default:
+      break;
     } // switch
 
   } // for
@@ -242,8 +293,8 @@ static void init_playfield_for_level(struct playfield *playfield, int level) {
   if (1 || level == 1) {
     *playfield = (struct playfield const)
       {
-       .num_arrays = 4,
-       .max_index_by_array = { 27, 19, 11, 9, 0 },
+       .num_arrays = 5,
+       .size_per_array = { 28, 20, 12, 4, 4 },
        .circular = 1,
        .spawn_next_nasty_timer = default_spawn_next_nasty_timer,
        .num_flippers = 8,
@@ -252,29 +303,32 @@ static void init_playfield_for_level(struct playfield *playfield, int level) {
     // zero out array
     memset(playfield->arrays, 0, sizeof(int) * MAX_PLAYFIELD_NUM_ARRAYS * MAX_PLAYFIELD_ARRAY_SIZE);
 
+    // TODO: delete / refactor code once spawning rules are created
+    // for now, manually spawn two flippers
+
     for (int i = 2; i < playfield->num_flippers; ++i) {
       // put flippers at max depth, inactive, and randomly positioned
       playfield->flippers[i].depth = playfield->num_arrays - 1;
       playfield->flippers[i].flipper_state = INACTIVE;
       playfield->flippers[i].position = rand() %
-        playfield->max_index_by_array[ playfield->num_arrays - 1 ];
+        playfield->size_per_array[ playfield->num_arrays - 1 ];
     }
 
     playfield->flippers[0].depth = playfield->num_arrays - 1;
-    playfield->flippers[0].flipper_state = SPAWNING;
+    playfield->flippers[0].flipper_state = ACTIVE;
     playfield->flippers[0].position = rand() %
-      playfield->max_index_by_array[ playfield->num_arrays - 1 ];
+      playfield->size_per_array[ playfield->num_arrays - 1 ];
     playfield->flippers[0].intra_depth_timer =
-      default_spawn_movement_timer + rand() % default_spawn_movement_timer_randomness;
+      default_spawn_move_timer + rand() % default_spawn_move_timer_randomness;
     playfield->flippers[0].next_depth_timer = default_next_depth_timer;
     playfield->flippers[0].movement_direction = rand() & 0b1 ? 1 : -1;
     
     playfield->flippers[1].depth = playfield->num_arrays - 1;
-    playfield->flippers[1].flipper_state = SPAWNING;
+    playfield->flippers[1].flipper_state = ACTIVE;
     playfield->flippers[1].position = rand() %
-      playfield->max_index_by_array[ playfield->num_arrays - 1 ];
+      playfield->size_per_array[ playfield->num_arrays - 1 ];
     playfield->flippers[1].intra_depth_timer =
-      default_spawn_movement_timer + rand() % default_spawn_movement_timer_randomness;
+      default_spawn_move_timer + rand() % default_spawn_move_timer_randomness;
     playfield->flippers[1].next_depth_timer = default_next_depth_timer;
     playfield->flippers[1].movement_direction = rand() & 0b1 ? 1 : -1;
 
@@ -294,25 +348,6 @@ static uint16_t player_glyph_green[] =
   { 0x2084, 0x0482, 0x0600, 0x0300,  0x0600, 0x0300,  0x0600, 0x0300,
     0x0600, 0x0300, 0x0160, 0x0850 };
 
-struct flipper_position_to_display_digit_and_glyph {
-  int display;
-  int digit;
-  uint16_t glyph;
-};
-
-static const struct flipper_position_to_display_digit_and_glyph flipper_glyph_by_depth3[] =
-  {
-   { 1, 1, 0x2000 },
-   { 1, 1, 0x0004,},
-   { 1, 2, 0x0010 },
-   { 1, 2, 0x0800 },
-   { 1, 2, 0x0040 },
-   { 1, 2, 0x0100 },
-   { 1, 2, 0x0020 },
-   { 1, 1, 0x0002 },
-   { 1, 1, 0x0400 },
-   { 1, 1, 0x0080 }
-  };
 
 
 // implements f_get_display for the red display
@@ -337,6 +372,10 @@ static display_type get_red_display(struct display_strategy *display_strategy, d
     (*value).display_glyph[3] = player_glyph_red_blue[this->player.position];
   }  
 
+  // draw any active flippers
+  for (int i = 0; i < MAX_FLIPPERS; ++i) {
+    display_flipper(&this->playfield.flippers[i], DISPLAY_RED, value);
+  }
 
   return glyph_display;
 }
@@ -357,15 +396,9 @@ static display_type get_blue_display(struct display_strategy *display_strategy, 
     (*value).display_glyph[0] = player_glyph_red_blue[this->player.position - 26];
   } 
 
-  // draw any spawning/active flippers
+  // draw any active flippers
   for (int i = 0; i < MAX_FLIPPERS; ++i) {
-    if ((this->playfield.flippers[i].flipper_state == SPAWNING ||
-        this->playfield.flippers[i].flipper_state == ACTIVE) &&
-        this->playfield.flippers[i].depth == 3) {
-      
-      (*value).display_glyph[ flipper_glyph_by_depth3[ this->playfield.flippers[i].position ].digit ] |=
-        flipper_glyph_by_depth3[ this->playfield.flippers[i].position ].glyph;
-    }
+    display_flipper(&this->playfield.flippers[i], DISPLAY_BLUE, value);
   }
 
   return glyph_display;
@@ -393,6 +426,11 @@ static display_type get_green_display(struct display_strategy *display_strategy,
     (*value).display_glyph[0] = player_glyph_green[this->player.position - 14];
   } 
 
+  // draw any active flippers
+  for (int i = 0; i < MAX_FLIPPERS; ++i) {
+    display_flipper(&this->playfield.flippers[i], DISPLAY_GREEN, value);
+  }
+
   return glyph_display;
 }
 
@@ -411,3 +449,156 @@ static display_type get_leds_display(struct display_strategy *display_strategy, 
 }
 
 
+
+
+struct flipper_position_to_display_digit_and_glyph {
+  display_t display;
+  int digit;
+  uint16_t glyph_active;
+  uint16_t glyph_move[3];
+};
+
+// this completely convoluted data structure starts at the center of
+// the playfield, each list starts at 9 o'clock and goes CCW around
+// from there.
+
+// lookup by flipper position for depth3 ("d3")
+static const struct flipper_position_to_display_digit_and_glyph flipper_glyph_by_position_d4[] =
+  {
+   { DISPLAY_BLUE, 1, 0x0004, { 0x0004, 0x0004, 0x0004 } },
+   { DISPLAY_BLUE, 2, 0x0010, { 0x0010, 0x0010, 0x0010 } },
+   { DISPLAY_BLUE, 2, 0x0020, { 0x0020, 0x0020, 0x0020 } },
+   { DISPLAY_BLUE, 1, 0x0002, { 0x0002, 0x0002, 0x0002 } }
+  };
+
+
+static const struct flipper_position_to_display_digit_and_glyph flipper_glyph_by_position_d3[] =
+  {
+   { DISPLAY_BLUE, 1, 0x1000, { 0x2000, 0x0080, 0x0004 } },
+   { DISPLAY_BLUE, 2, 0x1000, { 0x0800, 0x0040, 0x0010 } },
+   { DISPLAY_BLUE, 2, 0x0200, { 0x0100, 0x0040, 0x0020 } },
+   { DISPLAY_BLUE, 1, 0x0200, { 0x0400, 0x0080, 0x0002 } }
+  };
+
+static const struct flipper_position_to_display_digit_and_glyph flipper_glyph_by_position_d2[] =
+  // 12 elements
+  {
+   { DISPLAY_BLUE,  1, 0x0010, { 0x0008, 0x0800, 0x2000 } },
+   { DISPLAY_RED,   1, 0x0020, { 0x0001, 0x0100, 0x0040 } },
+   { DISPLAY_RED,   1, 0x0002, { 0x0001, 0x0200, 0x0400 } },
+   { DISPLAY_RED,   2, 0x0020, { 0x0001, 0x0100, 0x0040 } },
+   { DISPLAY_RED,   2, 0x0002, { 0x0001, 0x0200, 0x0400 } },
+   { DISPLAY_BLUE,  2, 0x0004, { 0x2000, 0x1000, 0x0800 } },
+   { DISPLAY_BLUE,  2, 0x0002, { 0x0400, 0x0200, 0x0100 } },
+   { DISPLAY_GREEN, 2, 0x4000, { 0x0004, 0x0080, 0x2000 } },
+   { DISPLAY_GREEN, 2, 0x0008, { 0x1000, 0x0800, 0x0040 } },
+   { DISPLAY_GREEN, 1, 0x2000, { 0x0004, 0x0080, 0x0004 } },
+   { DISPLAY_GREEN, 1, 0x0008, { 0x1000, 0x0800, 0x0040 } },
+   { DISPLAY_BLUE,  1, 0x0020, { 0x0001, 0x0100, 0x0400 } }
+  };
+
+
+static const struct flipper_position_to_display_digit_and_glyph flipper_glyph_by_position_d1[] =
+  // 20 elements
+  {
+   { DISPLAY_BLUE,  0, 0x0004, { 0x0040, 0x2000, 0x1000 } },
+   { DISPLAY_RED,   0, 0x0001, { 0x0200, 0x0400, 0x0002 } },
+   { DISPLAY_RED,   0, 0x0002, { 0x0080, 0x0400, 0x0200 } },
+   { DISPLAY_RED,   1, 0x0020, { 0x0100, 0x0200, 0x0040 } },
+   { DISPLAY_RED,   1, 0x0001, { 0x0200, 0x0400, 0x0080 } },
+   { DISPLAY_RED,   2, 0x0020, { 0x0100, 0x0200, 0x0040 } },
+   { DISPLAY_RED,   2, 0x0001, { 0x0200, 0x0400, 0x0080 } },
+   { DISPLAY_RED,   3, 0x0020, { 0x0040, 0x0100, 0x0200 } },
+   { DISPLAY_RED,   3, 0x0001, { 0x0200, 0x0100, 0x0040 } },
+   { DISPLAY_BLUE,  3, 0x0010, { 0x0800, 0x1000, 0x2000 } },
+   { DISPLAY_BLUE,  3, 0x0020, { 0x0100, 0x0200, 0x0400 } },
+   { DISPLAY_GREEN, 3, 0x0008, { 0x2000, 0x1000, 0x0800 } },
+   { DISPLAY_GREEN, 3, 0x0010, { 0x0040, 0x1000, 0x0800 } },
+   { DISPLAY_GREEN, 2, 0x0004, { 0x1000, 0x2000, 0x1000 } },
+   { DISPLAY_GREEN, 2, 0x0008, { 0x0010, 0x0800, 0x1000 } },
+   { DISPLAY_GREEN, 1, 0x0004, { 0x1000, 0x2000, 0x1000 } },
+   { DISPLAY_GREEN, 1, 0x0008, { 0x0010, 0x0800, 0x1000 } },
+   { DISPLAY_GREEN, 0, 0x0004, { 0x2000, 0x1000, 0x0800 } },
+   { DISPLAY_GREEN, 0, 0x0008, { 0x0800, 0x1000, 0x2000 } },
+   { DISPLAY_BLUE,  0, 0x0002, { 0x0400, 0x0200, 0x0100 } }
+  };
+
+
+static const struct flipper_position_to_display_digit_and_glyph flipper_glyph_by_position_d0[] =
+  // 28 elements
+  {
+   { DISPLAY_BLUE,  0, 0x0010, { 0x0800, 0x1000, 0x2000 } },
+   { DISPLAY_RED,   0, 0x0020, { 0x0100, 0x0200, 0x0400 } },
+   { DISPLAY_RED,   0, 0x0010, { 0x0040, 0x0800, 0x1000 } },
+   { DISPLAY_RED,   0, 0x0008, { 0x0800, 0x0040, 0x0080 } },
+   { DISPLAY_RED,   0, 0x4000, { 0x0004, 0x0080, 0x2000 } },
+   { DISPLAY_RED,   1, 0x0008, { 0x0800, 0x0040, 0x0080 } },
+   { DISPLAY_RED,   1, 0x4000, { 0x0004, 0x0080, 0x2000 } },
+   { DISPLAY_RED,   2, 0x0008, { 0x0800, 0x0040, 0x0080 } },
+   { DISPLAY_RED,   2, 0x4000, { 0x0004, 0x0080, 0x2000 } },
+   { DISPLAY_RED,   3, 0x0008, { 0x0800, 0x0040, 0x0080 } },
+   { DISPLAY_RED,   3, 0x4000, { 0x0004, 0x0080, 0x2000 } },
+   { DISPLAY_RED,   3, 0x0004, { 0x2000, 0x1000, 0x0800 } },
+   { DISPLAY_RED,   3, 0x0002, { 0x0400, 0x0200, 0x0100 } },
+   { DISPLAY_BLUE,  3, 0x0004, { 0x2000, 0x1000, 0x0800 } },
+   { DISPLAY_BLUE,  3, 0x0002, { 0x0400, 0x0200, 0x0100 } },
+   { DISPLAY_GREEN, 3, 0x0004, { 0x2000, 0x1000, 0x0800 } },
+   { DISPLAY_GREEN, 3, 0x0002, { 0x0400, 0x0200, 0x0100 } },
+   { DISPLAY_GREEN, 3, 0x0400, { 0x0001, 0x0200, 0x0080 } },
+   { DISPLAY_GREEN, 3, 0x0100, { 0x0001, 0x0200, 0x0040 } },
+   { DISPLAY_GREEN, 2, 0x0400, { 0x0001, 0x0200, 0x0080 } },
+   { DISPLAY_GREEN, 2, 0x0100, { 0x0001, 0x0200, 0x0040 } },
+   { DISPLAY_GREEN, 1, 0x0400, { 0x0001, 0x0200, 0x0080 } },
+   { DISPLAY_GREEN, 1, 0x0100, { 0x0001, 0x0200, 0x0040 } },
+   { DISPLAY_GREEN, 0, 0x0400, { 0x0001, 0x0200, 0x0080 } },
+   { DISPLAY_GREEN, 0, 0x0100, { 0x0001, 0x0200, 0x0040 } },
+   { DISPLAY_GREEN, 0, 0x0020, { 0x0100, 0x0200, 0x0400 } },
+   { DISPLAY_GREEN, 0, 0x0010, { 0x0800, 0x1000, 0x2000 } },
+   { DISPLAY_BLUE,  0, 0x0020, { 0x0100, 0x0200, 0x0400 } }
+  };
+
+
+
+static const struct flipper_position_to_display_digit_and_glyph *flipper_glyph_by_depth_position[] =
+  {
+   flipper_glyph_by_position_d0,
+   flipper_glyph_by_position_d1,
+   flipper_glyph_by_position_d2,
+   flipper_glyph_by_position_d3,
+   flipper_glyph_by_position_d4
+  };
+
+
+static void display_flipper(struct flipper *flipper, display_t display, display_value *value) {
+  int flipper_depth = flipper->depth;
+  int flipper_position = flipper->position;
+  int flipper_state = flipper->flipper_state;
+
+    
+  if (flipper_glyph_by_depth_position[flipper_depth][flipper_position].display == display) {
+    // trying for at least a little readability from nested lookups...
+
+    switch (flipper_state) {
+    case ACTIVE:      
+      (*value).display_glyph[ flipper_glyph_by_depth_position[flipper_depth][flipper_position].digit ] |=
+        flipper_glyph_by_depth_position[flipper_depth][flipper_position].glyph_active;
+      break;
+    case FLIPPING_FROM1:
+    case FLIPPING_FROM2:
+    case FLIPPING_FROM3:
+      (*value).display_glyph[ flipper_glyph_by_depth_position[flipper_depth][flipper_position].digit ] |=
+        flipper_glyph_by_depth_position[flipper_depth][flipper_position].glyph_move[flipper_state - FLIPPING_FROM1];
+      break;
+    case FLIPPING_TO3:
+    case FLIPPING_TO2:
+    case FLIPPING_TO1:
+      // reverse index the glyph_move array: start high, go low
+      (*value).display_glyph[ flipper_glyph_by_depth_position[flipper_depth][flipper_position].digit ] |=
+        flipper_glyph_by_depth_position[flipper_depth][flipper_position].glyph_move[-1 * (flipper_state - FLIPPING_TO1)];
+
+    default:
+      break;
+    }
+  }
+
+}
